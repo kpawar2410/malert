e69e863fe6fe7ac24da6389e38d7eed854c0f954
Kalyani Pawar <kpawar1@jhu.edu>
2020-02-04 19:58:48 -0500

    Update malert.sh

--- malert.sh
+++ malert.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
-#Instead of /root/malert/test, 
+#Instead of /root/malert/test/*, 
 #write the path for your own directory 
-#where you have saved the malicious files you'd like to test 
+#where you have saved the malicious files 
+#that you want to test
 
 for f in /root/malert/test/*; do
 	python app.py -f $f -y 1 -e 1; 
